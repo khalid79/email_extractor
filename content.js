a7aef53595ee65826a6bee7c2632b4449916e24b
RedaBenh <benh.reda@gmail.com>
2013-06-27 20:59:14 +0200

    fix bug on phone Regex ( close #1 )

--- content.js
+++ content.js
@@ -7,6 +7,7 @@ function extractEmails()
 
 function extractPhoneNumbers()
 {	
+	
 	var phoneNumberRegex = /((08|0 8)[0-9]{2}([\ \.-][0-9]{3}){2})|(\+33|00\ 33)[\ \.-]?[\ ?[0-9]]?([\ \.-]?[0-9]{2}){4}|0[1-9]{1}(([0-9]{2}){4})|([0-9]{2}([\ \.-][0-9]{2}){4})/igm;
 	return jQuery.unique( $('body').html().match(phoneNumberRegex));
 }

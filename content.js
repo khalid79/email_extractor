d8d7893ded4bf6ed08ae9f470352c968a9e472a6
RedaBenh <benh.reda@gmail.com>
2013-06-28 01:11:35 +0200

    add extract information from selected text in the page

--- content.js
+++ content.js
@@ -1,22 +1,46 @@
 
-function extractEmails()
+/////////////////// FROM ALL DOM ///////////////////////
+
+function extractEmails(selectionText)
 {
 	var emailRegex = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/igm;
-    return jQuery.unique( $('body').html().match(emailRegex) );
+
+	var emailsList = null;
+    
+    if (selectionText == null) 
+    	emailsList = $('body').html().match(emailRegex) ;
+    else
+    	emailsList =  $('<div>'+selectionText+'<div>').html().match(emailRegex) ;
+     
+     return (emailsList == null)? emailsList:jQuery.unique(emailsList);
+
 }
 
-function extractPhoneNumbers()
+
+function extractPhoneNumbers(selectionText)
 {	
-	
 	var phoneNumberRegex = /((08|0 8)[0-9]{2}([\ \.-][0-9]{3}){2})|(\+33|00\ 33)[\ \.-]?[\ ?[0-9]]?([\ \.-]?[0-9]{2}){4}|0[1-9]{1}(([0-9]{2}){4})|([0-9]{2}([\ \.-][0-9]{2}){4})/igm;
-	return jQuery.unique( $('body').html().match(phoneNumberRegex));
+	
+	var phonesList = null;
+
+	if (selectionText == null) 
+    	phonesList = $('body').html().match(phoneNumberRegex) ;
+    else
+    	phonesList =  $('<div>'+selectionText+'<div>').html().match(phoneNumberRegex) ;
+     
+     return (phonesList == null)? phonesList:jQuery.unique(phonesList);	
+	
+	
 }
+
+//////////////////////////////////////////////////////////
+
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	
 	//responde to extract emailAddresses call
 	if (request.method == "getEmails" ){
-		var emails = extractEmails();
+		var emails = extractEmails(null);
 		console.log("extractEmails() : "+emails);
 		if( emails != null && emails != ''){
 			sendResponse({data: emails});			
@@ -27,8 +51,8 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	}
 	
 	//responde to extract phoneNumber call
-	if (request.method == "getPhoneNumbers" ){
-		var phones = extractPhoneNumbers();
+	else if (request.method == "getPhoneNumbers" ){
+		var phones = extractPhoneNumbers(null);
 		console.log("extractPhoneNumbers() : "+phones);
 		if( phones != null && phones != ''){
 			sendResponse({data: phones});			
@@ -37,5 +61,21 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 		}
 		
 	}
+	else if (request.method == "getSelection"){
+		//sendResponse({data: window.getSelection().toString()});
+		if(window.getSelection().toString() == ''){
+  			sendResponse({data: ''}); // snub them.
+		}else{
+			var emailsFromSelection = extractEmails(window.getSelection().toString());
+			var phonesFromSelection = extractPhoneNumbers(window.getSelection().toString());
+			console.log("emailsFromSelection : "+emailsFromSelection);
+			console.log("phonesFromSelection : "+phonesFromSelection);
+				
+			sendResponse({data: {emails: emailsFromSelection, phones: phonesFromSelection }});			
+		
+		}
+		
+	}
+    
 
 });
--- popup.js
+++ popup.js
@@ -20,6 +20,10 @@ $(document).ready(function(){
 		else $('#contactWebsite').val('');		
 	});
 
+
+    // to know if the user has selected a text
+    var selectionCase = false;
+
 	// FIXME : Execute when the ationbrowser(icon) was clicked (not tab)
 	chrome.tabs.getSelected(null, function(tab) {
     	
@@ -28,44 +32,98 @@ $(document).ready(function(){
 		// fill source from  
     	$('#source').val(tab.url);
 
+		//get selection from content_script
+		chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function (response) {
+				
+				console.log("chrome.tabs.sendRequest: getSelection");
+				
+				if(response != null && response.data != ''){
+				  		
+				  	selectionCase = true;
+
+			  		var emailsFromSelection = response.data.emails;	
+			  		var phonesFromSelection = response.data.phones;	
+
+			  		if(emailsFromSelection != '' || phonesFromSelection != '' ){
+			  			
+						if(emailsFromSelection != null && emailsFromSelection != ''){
+							$('#contactEmail').val(emailsFromSelection.join(','));
+							chrome.browserAction.setBadgeText({'text': String(emailsFromSelection.length)});
+							if(emailsFromSelection.length>0)
+								chrome.browserAction.setBadgeBackgroundColor({color: "#32cd32"});
+						}else{
+							$('#contactEmail').val("email not found");
+							chrome.browserAction.setBadgeText({'text': '0'});
+						}
+
+						if(phonesFromSelection != null && phonesFromSelection != ''){
+					  		$('#contactPhoneNumber').val(phonesFromSelection.join(','));
+						}else{
+							$('#contactPhoneNumber').val("phone number not found");
+						}
+
+					}	
+		  		
+			  		
+				}
+
+			});
+
+		// wait some seconte to receive the getSelection respose from content_script
+		setTimeout(function() {
+
+			console.log("reponse getSelection extern :"+selectionCase);
+    		
+    		//
+	    	if (selectionCase == false ){
+	    		// call contentscript to get email addresses and fill email address field
+				chrome.tabs.sendRequest(tab.id, {method: "getEmails"}, function (response) {
+					
+					console.log("chrome.tabs.sendRequest: getEmails");
+					
+					console.log("reponse emails :"+xinspect(response));
+					
+					if(response != null && response.data != null){
+
+						emails = response.data;	
+				  		$('#contactEmail').val(emails.join(','));
+						chrome.browserAction.setBadgeText({'text': String(emails.length)});
+						if(emailsFromSelection.length>0)
+							chrome.browserAction.setBadgeBackgroundColor({color: "#32cd32"});
+					}else{
+						$('#contactEmail').val("email not found");
+						chrome.browserAction.setBadgeText({'text': '0'});
+					}
+					
+
+				});
+
+				// Call contentscript to get phone Numbers and fill phone number field
+				chrome.tabs.sendRequest(tab.id, {method: "getPhoneNumbers"}, function (response) {
+					
+					console.log("chrome.tabs.sendRequest: getPhoneNumbers");
+					
+					console.log("reponse phones :"+xinspect(response));
+					
+					if(response != null && response.data != null){
+
+						phones = response.data;	
+				  		$('#contactPhoneNumber').val(phones.join(','));
+					}else{
+						$('#contactPhoneNumber').val("phone number not found");
+					}
+					
+
+				});	
+	    	
+	    	}
+
+
+		},750);
+
+
     	
-		// call contentscript to get email addresses and fill email address field
-		chrome.tabs.sendRequest(tab.id, {method: "getEmails"}, function (response) {
-			
-			console.log("chrome.tabs.sendRequest: getEmails");
-			
-			console.log("reponse emails :"+xinspect(response));
-			
-			if(response != null && response.data != null){
-
-				emails = response.data;	
-		  		$('#contactEmail').val(emails.join(','));
-				chrome.browserAction.setBadgeText({'text': String(emails.length)});
-			}else{
-				$('#contactEmail').val("email not found");
-				chrome.browserAction.setBadgeText({'text': '0'});
-			}
-			
-
-		});
-
-		// Call contentscript to get phone Numbers and fill phone number field
-		chrome.tabs.sendRequest(tab.id, {method: "getPhoneNumbers"}, function (response) {
-			
-			console.log("chrome.tabs.sendRequest: getPhoneNumbers");
-			
-			console.log("reponse phones :"+xinspect(response));
-			
-			if(response != null && response.data != null){
-
-				phones = response.data;	
-		  		$('#contactPhoneNumber').val(phones.join(','));
-			}else{
-				$('#contactPhoneNumber').val("phone number not found");
-			}
-			
-
-		});
+		
 	});
 
 });
